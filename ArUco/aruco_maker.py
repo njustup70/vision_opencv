36bcfa539ab7d3e3add7b4d89112e1951682fc00
SummerWen-Lab <dalita88alitadom@hotmail.com>
2025-03-19 23:07:41 +0800

    将原始代码封装成了ArUco类，支持图像和视频检测以及快速ArUco码生成。以及提供了一个ArUco类的使用示例脚本。

--- ArUco/aruco_maker.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_7X7_1000)
-maker = cv2.aruco.generateImageMarker(aruco_dict, 29, 400)
-
-cv2.imwrite("test_7x7_1000_29.jpg",maker)
