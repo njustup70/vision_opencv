303dcaa51c55c99f542ae73b629130fa78eb0562
Elaina <1463967532@qq.com>
2025-04-02 22:25:07 +0800

    feat增加ros2节点代码,遇到python版本冲突

---
+++ src/cv_lib/cv_ros_lib.py
@@ -0,0 +1,53 @@
+import rclpy
+import cv2
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+class ImagePublish_t:
+    """
+        将opencv图像发布到ROS 2话题
+        :param node: ROS 2节点对象
+        :param topic: 话题名称
+        :param queue_size: 消息队列大小
+    """
+    def __init__(self,node:rclpy.Node, topic:str,queue_size:int=10):
+        self._node = node
+        self._topic = topic
+        self._queue_size = queue_size
+        self._image_publish={}
+        self._publisher=self._node.create_publisher(Image, self._topic, self._queue_size)
+        self._bridge = CvBridge()
+        # self._copy = copy
+    def update(self, image:cv2.Mat,content:dict):
+        # self.node.get_logger().info(f"Publishing image to {self.topic}")
+        """
+        将 OpenCV 图像发布到 ROS 2 话题
+        :param image: OpenCV 图像对象 (np.ndarray)
+        :param content: 附加的内容，如时间戳、坐标系等
+        """
+        ros_image = self._bridge.cv2_to_imgmsg(image, encoding="bgr8")
+        #默认复制传入图像的header
+        ros_image.header=content['header']
+        # 发布图像消息
+        self._publisher.publish(ros_image)
+class ImageConver_t:
+    """
+        将ROS 2图像转换为OpenCV格式
+        :param node: ROS 2节点对象
+        :param topic: 话题名称
+        :param queue_size: 消息队列大小
+    """
+    def __init__(self,use_compression=False):
+        self.bridge = CvBridge()
+        self.use_compression = use_compression
+    def update(self, image_msg:Image):
+        """
+        将 ROS 2 图像消息转换为 OpenCV 格式
+        :param image_msg: ROS 2 图像消息
+        :return: OpenCV 图像对象 (np.ndarray)
+        """
+        if self.use_compression:
+            # 使用压缩格式
+            return self.bridge.compressed_imgmsg_to_cv2(image_msg, desired_encoding="passthrough")
+        else:
+            # 使用未压缩格式
+            return self.bridge.imgmsg_to_cv2(image_msg, desired_encoding="passthrough")
