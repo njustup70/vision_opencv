a0b4b8efd39ac0781b93bdde9763f02ef97a45e0
Elaina <1463967532@qq.com>
2025-04-07 14:55:53 +0000

    feat(aruco)更改代码适配打印的aruco码

--- cv_node/aruco_detect.py
+++ cv_node/aruco_detect.py
@@ -3,7 +3,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) 
 print(sys.path)
-
+import time
 import cv2
 import numpy as np
 from cv_lib.cv_bridge import ImagePublish_t,ImageReceive_t
@@ -12,16 +12,22 @@ from ArUco.Aruco import Aruco
 def main():
     
     pipe=[]
-    pipe.append(ImageReceive_t())
-    pipe.append(Aruco("DICT_4X4_50"))
+    pipe.append(ImageReceive_t(print_latency=True))
+    pipe.append(Aruco("DICT_5X5_100",if_draw=True))
     pipe.append(ImagePublish_t("aruco"))
     content={}
+    print_time=True
     while True:
         # 创建一个空的图像对象，这里用一个全黑的图像作为示例
         image = np.zeros((1, 1, 3), dtype=np.uint8)
         #启动处理
         for p in pipe:
+            if print_time:
+                start_time = time.time()
             p.update(image,content)
-        print(content)
+            if print_time:
+                end_time = time.time()
+                print(f"name:{type(p).__name__}: {(end_time - start_time)*1000:2f} ms")
+                print(content)
 if __name__ == "__main__":
     main()
