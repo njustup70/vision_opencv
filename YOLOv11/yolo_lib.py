94acca8c1aa3a7454dae03b7dc3d969f4e1bc052
Elaina <1463967532@qq.com>
2025-04-11 15:46:00 +0000

    feat(yolo)增加Yolo输出可视化

--- YOLOv11/yolo_lib.py
+++ YOLOv11/yolo_lib.py
@@ -6,5 +6,20 @@ class MyYOLO():
         self.model = YOLO(model_path)
         self.show=show
     def update(self,image:np.ndarray,content:dict):
-        results=self.model(image,self.show)
-        # results.print()
+        results=self.model(image)
+        # 把结果显示到图片上
+        #创建一个空的图像对象
+        segmentation_mask = np.zeros_like(image, dtype=np.uint8)
+        for i,result in enumerate(results):
+            if result.masks is None:
+                continue
+            for j,mask in enumerate(result.masks.xy):
+                mask=np.array(mask,dtype=np.int32)
+                segmentation_mask=cv2.fillPoly(segmentation_mask, [mask], (0, 255, 0))
+        if self.show:
+            # cv2.addWeighted(image, 1, segmentation_mask, 0.7, 0, image)  # 将修改写回原图像
+            result = results[0]
+            image_result= result.plot()
+            #将image_result内容给image
+            image[:]=image_result
+            
