1de956c642cad375974019f684a00882d7e2fc72
Elaina <1463967532@qq.com>
2025-04-11 10:06:45 +0000

    feat(docker)增加了cpu的docker 将yolo的图像显示默认关了

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,1 +1,10 @@
-docker build -t elaina/yolo_image .
+#!/bin/bash
+
+# 检查是否传入参数
+if [ "$1" == "--cpu" ]; then
+    # 如果是 --cpu 参数，使用 cpu.dockerfile
+    docker build -t elaina/yolo_image -f cpu.dockerfile .
+else
+    # 默认情况，使用 Dockerfile（假设该文件是默认的）
+    docker build -t elaina/yolo_image .
+fi
--- YOLOv11/yolo_lib.py
+++ YOLOv11/yolo_lib.py
@@ -2,8 +2,9 @@ from ultralytics import YOLO
 import numpy as np
 import cv2
 class MyYOLO():
-    def __init__(self, model_path):
+    def __init__(self, model_path,show=False):
         self.model = YOLO(model_path)
+        self.show=show
     def update(self,image:np.ndarray,content:dict):
-        results=self.model(image,show=True)
+        results=self.model(image,self.show)
         # results.print()
