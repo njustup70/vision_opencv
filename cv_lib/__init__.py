b9a640cf542e920325e98e8221a25b4a42be2e45
Elaina <1463967532@qq.com>
2025-04-27 14:39:38 +0000

    fix(path)修复导入路径问题

--- cv_lib/__init__.py
+++ cv_lib/__init__.py
@@ -1,4 +1,4 @@
-from ros.cv_bridge import ImagePublish_t, ImageReceive_t
+from .ros.cv_bridge import ImagePublish_t, ImageReceive_t
 from PoseSolver.PoseSolver import PoseSolver
 from PoseSolver.Aruco import Aruco
 from YOLOv11.yolo_lib import MyYOLO
