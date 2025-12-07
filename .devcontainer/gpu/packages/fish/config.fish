086f854a97a4c8868bd4fa19e465aab0a7b3549f
Elaina <84247003+Elaina-rascal@users.noreply.github.com>
2025-12-08 00:05:24 +0800

    维护镜像，更改ros 话题接收 (#23)
    * 修改镜像，移至docker 仓库维护
    
    * 修改为数据卷
    
    * 更改ros bridge
    
    * 将aruco码通信移入test文件夹

--- .devcontainer/gpu/packages/fish/config.fish
+++
@@ -1,8 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-bass source /opt/ros/humble/setup.bash
-bass source ~/.bashrc
-
- 
-# thefuck --alias | source
